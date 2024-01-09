2f81dc4c088a63631a880970174ee5ae62fb57ce
Xeno Dev <118352934+xenodeve@users.noreply.github.com>
2024-01-09 16:40:17 +0700

    Update nodeDisconnect.js

--- src/events/erelajs/nodeDisconnect.js
+++ src/events/erelajs/nodeDisconnect.js
@@ -2,7 +2,8 @@ module.exports = {
 	name: 'nodeDisconnect',
 	execute(node, player) {
 		console.log('[WARN] Node', node.options.identifier, 'disconnected.');
-		player.disconnect();
+		await 
 		player.queue.clear();
+		player.destroy();
 	},
 };
