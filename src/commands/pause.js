cd6cca5554ad16c0eb46c0ef7e9f84c1fb3f1095
Xeno Dev <118352934+xenodeve@users.noreply.github.com>
2024-01-09 22:29:34 +0700

    add pause & resume

---
+++ src/commands/pause.js
@@ -0,0 +1,55 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const config = require('../settings/config.json');
+const { red } = require('color-name');
+const { convertTime}  = require('../structures/convertTime.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('pause')
+        .setDescription('พัก'),
+    async execute(interaction) {
+        player = global.player;
+
+        if (!player) {
+            const embed = new EmbedBuilder()
+                .setColor(red)
+                .setDescription(`> ❌ไม่มีเครื่องเล่น`);
+
+            return interaction.reply({ embeds: [embed], ephemeral: true });
+        } else if (!player.playing) {
+            const embed = new EmbedBuilder()
+                .setColor(red)
+                .setDescription(`> ❌ไม่มีเพลงที่เล่นอยู่`);
+
+            return interaction.reply({ embeds: [embed], ephemeral: true });
+        } else if (!interaction.member.voice.channel) {
+            const embed = new EmbedBuilder()
+                .setColor(red)
+                .setDescription(`> ❌กรุณาเข้าห้องเสียงด้วย`);
+
+            return interaction.reply({ embeds: [embed], ephemeral: true });
+        } else if (interaction.member.voice.channel.id !== player.voiceChannel) {
+            const embed = new EmbedBuilder()
+                .setColor(red)
+                .setDescription(`> ❌คุณต้องอยู่ในห้องเดียวกับบอท`);
+
+            return interaction.reply({ embeds: [embed], ephemeral: true });
+        }
+
+        await interaction.deferReply({ ephemeral: false });
+
+        await player.pause(player.playing);
+
+        const video_id = player.queue.current.uri.split('v=')[1];
+        const userAvatar = interaction.user.displayAvatarURL();
+
+        const embed = new EmbedBuilder()
+            .setColor(config.embed_color)
+            .setAuthor({ name: 'Go to Page', iconURL: userAvatar, url: player.queue.current.uri})
+            .setDescription(`⏸️┃**${player.queue.current.title}** \` ${convertTime(player.queue.current.duration)} \``)
+            .setThumbnail(`https://img.youtube.com/vi/${video_id}/maxresdefault.jpg`)
+            .setFooter({ text: 'พักการเล่น' })
+
+        return interaction.editReply({ embeds: [embed] });
+    }
+}
