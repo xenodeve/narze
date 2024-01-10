18ed28ab2820f4374c5b9de56e46d0da74f74ed6
Xeno Dev <118352934+xenodeve@users.noreply.github.com>
2024-01-10 10:00:56 +0700

    update chalk

--- src/events/erelajs/nodeReconnect.js
+++ src/events/erelajs/nodeReconnect.js
@@ -3,6 +3,6 @@ const chalk = require('chalk');
 module.exports = {
 	name: 'nodeReconnect',
 	execute(node) {
-	console.log(`[${chalk.bold.yellowBright('WARN')}] ${node.options.identifier} ${chalk.yellowBright('reconnected.')}`);
+	console.log(`[${chalk.bold.yellowBright('WARN')}] ${node.options.identifier} ${chalk.greenBright('reconnected.')}`);
 	},
 };
