7b02e09b2f55d66d86bbf4fa1635bbe08303b739
Xeno Dev <118352934+xenodeve@users.noreply.github.com>
2024-01-12 00:53:11 +0700

    update trackEnd.js

--- src/events/erelajs/trackEnd.js
+++ src/events/erelajs/trackEnd.js
@@ -1,6 +1,4 @@
 const { EmbedBuilder } = require('discord.js');
-const config = require('../../settings/config.json');
-const { convertTime } = require('../../structures/convertTime.js');
 
 module.exports = {
     name: 'trackEnd',
@@ -15,13 +13,13 @@ module.exports = {
         if (autoplay === true) {
             const requester = player.get('requester');
             const identifier = player.queue.current.identifier;
-            console.log('คิว1 : ',player.queue.length, player.queue.size, player.queue.totalSize, identifier)
+            // console.log('คิว1 : ',player.queue.length, player.queue.size, player.queue.totalSize, identifier)
             const search = `https://www.youtube.com/watch?v=${identifier}&list=RD${identifier}`;
             let res = await player.search(search, requester);
             // console.log('autoplay', autoplay, 'requester', requester, 'identifier', identifier, 'search', search, 'res', res)
             try {
                 await player.queue.add(res.tracks[1]);
-                console.log('คิว2 : ',player.queue.length, player.queue.size, player.queue.totalSize, identifier)
+                // console.log('คิว2 : ',player.queue.length, player.queue.size, player.queue.totalSize, identifier)
             } catch (e) {
                 const embed = new EmbedBuilder()
                 .setDescription(`> ❌Autoplay Support Youtubeเท่านั้น`)
